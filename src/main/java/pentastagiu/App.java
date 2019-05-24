c3a910ecd8a37beb91add72ffa907f21e0cbe1a7
Ovidiu Ionut <bujorovidiuionut@gmail.com>
2019-05-24 09:14:51 +0300

    Spring Web Services - Resolved generating table in database.

--- src/main/java/pentastagiu/App.java
+++ src/main/java/pentastagiu/App.java
@@ -4,16 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-//import static pentastagiu.util.Constants.FACTORY;
-
 /**
  * This class is the Main class of the project and the starting point.
  */
 @SpringBootApplication
 @EnableJpaRepositories
 public class App {
-
-//    private static Logger LOGGER = LogManager.getLogger();
 
     /**
      * Main method that starts the application.
@@ -25,19 +21,6 @@ public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
 
-//        UserService userService = new UserService();
-//        LOGGER.info("Session created");
-//        userService.createInitialsUsers();
-//        try {
-//            DisplayService.InitialMenu();
-//            LoginService processUserInput = new LoginService();
-//            UserService cachedUser = new UserService();
-//            processUserInput.beginProcessing(cachedUser);
-//        }
-//        finally {
-//            LOGGER.info("Closing SessionFactory");
-////            FACTORY.close();
-//        }
     }
 
 }
--- src/main/java/pentastagiu/repository/AccountRepository.java
+++ src/main/java/pentastagiu/repository/AccountRepository.java
@@ -1,8 +1,5 @@
 package pentastagiu.repository;
 
-//import org.hibernate.Session;
-//import org.hibernate.resource.transaction.spi.TransactionStatus;
-
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import pentastagiu.model.Account;
