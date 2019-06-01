f66f970b59e0fcd11f6e7412bd5f9f4f8eb46f85
Ovidiu Ionut <bujorovidiuionut@gmail.com>
2019-06-01 20:17:23 +0300

    Added first test.

---
+++ src/test/java/pentastagiu/services/UserServiceTest.java
@@ -0,0 +1,46 @@
+package pentastagiu.services;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import pentastagiu.model.User;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class UserServiceTest {
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @Autowired
+    private UserService userService;
+    @Test
+    void getUserById() {
+        User user = authenticationService.findByToken("jCHk8ui2faMP5Tpu3mbb").getUser();
+        assertEquals(user ,userService.getUserById((long) 1));
+    }
+
+    @Test
+    void createUser() {
+
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void deleteUserById() {
+    }
+
+    @Test
+    void validateUser() {
+    }
+
+    @Test
+    void getUserByUsername() {
+    }
+}
